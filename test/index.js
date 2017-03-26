283f015734711a74b4f84f3467c6b706e207d1f6
Kiko Beats <josefrancisco.verdu@gmail.com>
2017-03-26 14:57:33 +0200

    Refactor tests

--- test/index.js
+++ test/index.js
@@ -5,23 +5,26 @@ const should = require('should')
 const directory = require('..')
 
 describe('directory', function () {
-  it('nothing to detect', function () {
-    const { data } = directory('')
-    should(data).be.eql({})
+  it('under non detection, output is the string input', function () {
+    const result = directory('foo')
+    should(result).be.eql({
+      data: {},
+      output: 'foo'
+    })
   })
 
   describe('only detect brand', function () {
-    const expectedData = {brand: 'Chinook'}
-    const expectedOutput = ' PRO 1 ALLOY BOOM'
     ;[
       'CHINOOK PRO 1 ALLOY BOOM',
       'chinook PRO 1 ALLOY BOOM',
       'Chinook PRO 1 ALLOY BOOM'
     ].forEach(function (str) {
       it(`${str} → Chinook`, function () {
-        const { data, output } = directory(str)
-        should(data).be.eql(expectedData)
-        should(output).be.equal(expectedOutput)
+        const result = directory(str)
+        should(result).be.eql({
+          data: {brand: 'Chinook'},
+          output: ' PRO 1 ALLOY BOOM'
+        })
       })
     })
   })
